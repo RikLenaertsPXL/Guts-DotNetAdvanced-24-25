451860ce4f9b93cedbec1914e6c363ff7ecd5b45
RikLenaertsPXL <rik.lenaerts@student.pxl.be>
2024-11-19 17:24:40 +0100

    added tests RomanNumber exercise

--- 2_MVC_EF_NUnit/RomanNumberConverter/RomanNumberConverterApp.Ui.Tests/Models/RomanNumberConverterTests.cs
+++ 2_MVC_EF_NUnit/RomanNumberConverter/RomanNumberConverterApp.Ui.Tests/Models/RomanNumberConverterTests.cs
@@ -4,14 +4,31 @@ namespace RomanNumberConverterApp.Ui.Tests.Models
 {
     public class RomanNumberConverterTests
     {
-        public void Convert_ValueIsNotBetweenOneAnd3999_ShouldThrowArgumentException()
+        private RomanNumberConverter _sut;
+
+        [SetUp]
+        public void BeforeEachTest()
         {
-            Assert.Fail("Test not implemented yet.");
+            _sut = new RomanNumberConverter();
         }
 
-        public void Convert_ValidValue_ShouldReturnRomanNumberEquivalent()
+        [TestCase(0)]
+        [TestCase(4000)]
+        public void Convert_ValueIsNotBetweenOneAnd3999_ShouldThrowArgumentException(int number)
         {
-            Assert.Fail("Test not implemented yet.");
+            Assert.That(() => _sut.Convert(number), Throws.TypeOf<ArgumentException>());
+        }
+
+        [TestCase(1986, "MCMLXXXVI")]
+        [TestCase(52, "LII")]
+        [TestCase(174, "CLXXIV")]
+        [TestCase(11, "XI")]
+        [TestCase(3874, "MMMDCCCLXXIV")]
+        public void Convert_ValidValue_ShouldReturnRomanNumberEquivalent(int number, string expectedResult)
+        {
+            string result = _sut.Convert(number);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
         }
     }
 }
